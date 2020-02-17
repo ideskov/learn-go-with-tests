225e99bc0f50eb279b88ceaf77c1a076353c058f
Ivan Deskov <ivan.deskov@aexp.com>
2020-02-17 20:24:35 +0000

    Determine the language in a separate function and use a switch statement

--- hello/hello.go
+++ hello/hello.go
@@ -12,15 +12,21 @@ func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	if language == spanish {
-		return spanishHelloPrefix + name
-	}
-	if language == french {
-		return frenchHelloPrefix + name
-	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
 }
 
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
 func main() {
 	fmt.Println(Hello("world", ""))
 }
