b553e6046324386a14d7141b2abb19bbb8be7ff0
Ivan Deskov <ivan.deskov@aexp.com>
2020-02-19 09:09:54 +0000

    Implement withdraw function and 'toString' on the Bitcoin

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,6 +1,12 @@
 package pointers
 
+import "fmt"
+
 type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
 
 type Wallet struct {
 	balance Bitcoin
@@ -8,6 +14,10 @@ type Wallet struct {
 
 func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
 }
 
 func (w *Wallet) Balance() Bitcoin {
