1e7d0874a35157aa42c2e4b40e4939333b80cb87
Ivan Deskov <ivan.deskov@aexp.com>
2020-02-19 09:36:36 +0000

    Refactor tests

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -12,6 +12,13 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
+	assertError := func(t *testing.T, err error) {
+		t.Helper()
+		if err == nil {
+			t.Error("Wanted an error but didn't get one")
+		}
+	}
+
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
@@ -30,9 +37,7 @@ func TestWallet(t *testing.T) {
 		err := wallet.Withdraw(Bitcoin(100))
 
 		assertBalance(t, wallet, startingBalance)
-		if err == nil {
-			t.Error("Wanted an error but didn't get one")
-		}
+		assertError(t, err)
 	})
 
 }
