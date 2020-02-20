97448d2447df6d8ee6af0e1f653e84160c758478
Ivan Deskov <ivan.deskov@aexp.com>
2020-02-20 12:26:44 +0000

    Add sleeping between countdowns

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -4,13 +4,20 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
+)
+
+const (
+	finalWord      = "Go!"
+	countdownStart = 3
 )
 
 func Countdown(writer io.Writer) {
-	for i := 3; i > 0; i-- {
+	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(writer, i)
+		time.Sleep(time.Second)
 	}
-	_, _ = fmt.Fprint(writer, "Go!")
+	_, _ = fmt.Fprint(writer, finalWord)
 }
 
 func main() {
