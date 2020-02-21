76f851719cdd8db0046731ed000d868a2a54fd89
Ivan Deskov <ivan.deskov@aexp.com>
2020-02-21 14:26:39 +0000

    Timeout after 10s

--- select/WebsiteRacer_test.go
+++ select/WebsiteRacer_test.go
@@ -9,21 +9,38 @@ import (
 
 func TestRacer(t *testing.T) {
 
-	slowServer := startServer(20 * time.Millisecond)
-	fastServer := startServer(0)
+	t.Run("quicker server wins", func(t *testing.T) {
+		slowServer := startServer(20 * time.Millisecond)
+		fastServer := startServer(0)
 
-	defer slowServer.Close()
-	defer fastServer.Close()
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+		want := fastURL
+		got, _ := Racer(slowURL, fastURL)
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+		serverA := startServer(11 * time.Second)
+		serverB := startServer(12 * time.Second)
+
+		defer serverA.Close()
+		defer serverB.Close()
+
+		_, err := Racer(serverA.URL, serverB.URL)
+
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
+	})
+
 }
 
 func startServer(responseDelay time.Duration) *httptest.Server {
