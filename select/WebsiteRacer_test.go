97e1eea9423f6e5c22c94f0cbc143eb53fdd6b46
Ivan Deskov <ivan.deskov@aexp.com>
2020-02-21 14:44:53 +0000

    Maker racer configurable for quicker tests

--- select/WebsiteRacer_test.go
+++ select/WebsiteRacer_test.go
@@ -20,7 +20,11 @@ func TestRacer(t *testing.T) {
 		fastURL := fastServer.URL
 
 		want := fastURL
-		got, _ := Racer(slowURL, fastURL)
+		got, err := Racer(slowURL, fastURL)
+
+		if err != nil {
+			t.Errorf("did not expect an error but got one %v", err)
+		}
 
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
@@ -28,13 +32,11 @@ func TestRacer(t *testing.T) {
 	})
 
 	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
-		serverA := startServer(11 * time.Second)
-		serverB := startServer(12 * time.Second)
+		server := startServer(10 * time.Millisecond)
 
-		defer serverA.Close()
-		defer serverB.Close()
+		defer server.Close()
 
-		_, err := Racer(serverA.URL, serverB.URL)
+		_, err := ConfigurableRacer(server.URL, server.URL, 5*time.Millisecond)
 
 		if err == nil {
 			t.Error("expected an error but didn't get one")
